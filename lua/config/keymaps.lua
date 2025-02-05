5cbe89bab8921ebebf1704f4dcb0420c7b079a6d
单子亨 <2366910832@qq.com>
2025-02-05 00:48:54 +0000

    Added support for vim-tmux-navigator

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- vim-tmux-navigator
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "Go to Right Window", remap = true })
